af5808bb627988efda2e6267db1e60d972545b3d
MishoK1997 <misho.khojadze@gmail.com>
2025-02-01 22:56:22 +0400

    I've done a lot, ectually I need to make resposible this webpage and add copy button

--- script.js
+++ script.js
@@ -37,3 +37,21 @@ console.log(newStr.charAt(2))
 let fruits = "apple,banana,grape,orange";
 let fruitArray = fruits.split(",");
 console.log(fruitArray);
+
+
+const reducedVal = [2,3,4,5,6].reduce((total, currVal)=> total+currVal, 0);
+console.log(reducedVal)
+
+
+const someVal = ["a", "b", "c", "A", "d"].some(letter => letter == letter.toUpperCase());
+
+console.log(someVal)
+
+console.log("Say Hello to JS".indexOf("JS",0))
+
+
+console.log("Mama world".includes("world", 6))
+
+/**
+ * Repetiiton secton above -------------------------------
+ */
