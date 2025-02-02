d36d198ce3afc71a2a9bacdbdde4d736f0041589
MishoK1997 <misho.khojadze@gmail.com>
2025-02-02 17:09:30 +0400

    I've accomplished this project at this state, new thing is coppy button, that I've added via DOM from JS file.

--- script.js
+++ script.js
@@ -1,57 +1,31 @@
 
-const filter_list = list =>  list.filter(el=> Number.isInteger(el))
-const secondFilter_list = list => list.filter(el=> typeof el === 'number')
+// Add and set copy cplipboard functionality 
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.querySelectorAll("pre").forEach(pre=>{
+        let button = document.createElement("button");
+        button.innerText ="Copy";
+        button.classList.add("copy-btn");
+
+        // pre.style.position = "relative";
+        pre.appendChild(button)
 
 
-console.log("checking================================")
+        // Copy functionality
 
-console.log(filter_list([1,2,"22","string",100]))
+        button.addEventListener("click", ()=>{
+            let code = pre.querySelector("code").innerText;
 
-console.log(filter_list([1,2,'aasf','1','123',123]))
+            navigator.clipboard.writeText(code).then(()=>{
+                button.innerText = "Copied!";
+                button.classList.add("copied")
 
+                setTimeout(()=> {
+                    button.innerText = "Copy";
+                    button.classList.remove("copied")
+                }, 2000)
+            }).catch(err=> console.error(err))
+        })
+    })
+})
 
-const items = ["key", "toy", "notebook", "car"];
-
-console.log(items.slice(-3,-1))
-
-console.log("Another block ------------------------------")
-
-const rangeArr = ( start,end) =>  Array(end - start +1)
-.fill(start).map((el,ind)=>el+ind)
-
-
-console.log(rangeArr(10,20))
-
-
-const myStrNum = "This is num 2 300 ss"
-
-const parsedVal = Number.parseFloat(myStrNum)
-
-console.log(parsedVal)
-
-const newStr = "This is a string";
-
-console.log(newStr.charAt(2))
-
-
-let fruits = "apple,banana,grape,orange";
-let fruitArray = fruits.split(",");
-console.log(fruitArray);
-
-
-const reducedVal = [2,3,4,5,6].reduce((total, currVal)=> total+currVal, 0);
-console.log(reducedVal)
-
-
-const someVal = ["a", "b", "c", "A", "d"].some(letter => letter == letter.toUpperCase());
-
-console.log(someVal)
-
-console.log("Say Hello to JS".indexOf("JS",0))
-
-
-console.log("Mama world".includes("world", 6))
-
-/**
- * Repetiiton secton above -------------------------------
- */
